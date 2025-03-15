2d7fbce6a7f116bfa66e6a542dd5362724c28958
Augusto Camati <augustocamati21@gmail.com>
2025-03-15 05:39:43 +0100

    alterando knexfile

--- knexfile.js
+++ knexfile.js
@@ -35,19 +35,14 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: "postgresql",
+    connection: process.env.PG_CONNECTION_STRING,
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+      tableName: "knex_migrations",
+    },
+  },
+}
