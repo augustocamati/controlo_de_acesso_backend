6894114fe3d43137dbd866aa990d56a3c6c0d5db
Augusto camati <auustocamati21@gmail.com>
2025-03-28 22:45:41 +0000

    adicionando rota de teste na aplicação

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,6 +7,12 @@ import logRoutes from "./logRoutes"
 
 const router = Router()
 // router.use("/auth", authRoutes)
+
+router.get("/", async (req, res) => {
+ 
+    res.status(200).json({ message: "Rota de teste" })
+  
+})
 router.use("/pacientes", pacienteRoutes)
 router.use("/funcionarios", funcionarioRoutes)
 router.use("/visitantes", visitanteRoutes)
