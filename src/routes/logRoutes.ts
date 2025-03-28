27b075a873b7dccd6d04fee002153fc077492462
Augusto camati <auustocamati21@gmail.com>
2025-03-28 22:45:50 +0000

    adicionando campo quartoId ao modelo Paciente e ajustando formatação no arquivo logRoutes.ts

--- src/routes/logRoutes.ts
+++ src/routes/logRoutes.ts
@@ -2,7 +2,7 @@ import { Router } from "express"
 import { PrismaClient } from "@prisma/client"
 
 const router = Router()
-const prisma = new PrismaClient()
+const prisma = new PrismaClient() 
 
 router.get("/", async (req, res) => {
   try {
