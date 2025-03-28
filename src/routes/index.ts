ef70531849ba4490f5dca8720909fb13e1c8347a
Augusto camati <auustocamati21@gmail.com>
2025-03-28 23:12:17 +0000

    adicionando rota de permissões ao roteador principal

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express"
 // import authRoutes from "./authRoutes"
 import pacienteRoutes from "./pacienteRoutes"
+import permissoesRoutes from "./permissoesRoutes"
 import funcionarioRoutes from "./funcionarioRoutes"
 import visitanteRoutes from "./visitanteRoutes"
 import logRoutes from "./logRoutes"
@@ -13,6 +14,7 @@ router.get("/", async (req, res) => {
     res.status(200).json({ message: "Rota de teste" })
   
 })
+router.use("/permissoes", permissoesRoutes)
 router.use("/pacientes", pacienteRoutes)
 router.use("/funcionarios", funcionarioRoutes)
 router.use("/visitantes", visitanteRoutes)
