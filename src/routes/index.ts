3d9a12b4afd3a116d18ac194e60b73d601c4afa3
Augusto camati <auustocamati21@gmail.com>
2025-03-31 00:50:48 +0100

    adicionando rota para gerenciamento de quartos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import pacienteRoutes from "./pacienteRoutes"
 import permissoesRoutes from "./permissoesRoutes"
 import funcionarioRoutes from "./funcionarioRoutes"
 import visitanteRoutes from "./visitanteRoutes"
+import quartoRoutes from "./quartoRoutes"
 import logRoutes from "./logRoutes"
 
 const router = Router()
@@ -18,6 +19,7 @@ router.use("/permissoes", permissoesRoutes)
 router.use("/pacientes", pacienteRoutes)
 router.use("/funcionarios", funcionarioRoutes)
 router.use("/visitantes", visitanteRoutes)
+router.use("/quartos", quartoRoutes)
 router.use("/logs", logRoutes)
 
 export default router
